e788fdcc1d738761f7ef0340af25b796d450283e
giri-j <jek5797@naver.com>
2026-04-30 10:20:24 +0900

    V260430.2

--- src/app/wedding/page.tsx
+++ src/app/wedding/page.tsx
@@ -123,7 +123,7 @@ const AccountInfo = () => {
   ];
 
   const brideAccounts = [
-    { name: '신부 조인아', bank: '우리은행', number: '1002-123-456789' },
+    { name: '신부 조인아', bank: '하나은행', number: '222-910235-10507' },
     { name: '신부측 혼주 조종섭', bank: '기업은행', number: '123-456-789012' },
     { name: '신부측 혼주 윤원흥', bank: '기업은행', number: '123-456-789012' },
   ];
